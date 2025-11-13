6e2104fd8610f95c7e9a580f808b439bc8eefcfd
Kai Wang <89493483+RogerWK@users.noreply.github.com>
2025-11-13 22:38:16 +1100

    Update data_processing.py

--- app/data_processing.py
+++ app/data_processing.py
@@ -9,7 +9,7 @@ def load_excel_data(filepath: str):
     constituents_df = pd.read_excel(filepath, sheet_name="IndexConstituents")
     return returns_df, constituents_df
 
-# Three strategies to handle nan value based on inputs and to calculate the cumulative geometric mean
+# Strategies to handle nan value based on inputs and to calculate the cumulative geometric mean
 # na_strategy options: 'keep', 'zero' and 'drop'
 def geometric_cumulative_return(returns: pd.Series, na_strategy: str = "keep") -> float:
     if na_strategy == "zero":
@@ -19,16 +19,16 @@ def geometric_cumulative_return(returns: pd.Series, na_strategy: str = "keep") -
     if returns.isna().any() or len(returns) == 0:
         return np.nan
         
-    #https://www.investopedia.com/terms/g/geometricmean.asp  
-    # μ geometric=[(1+R1)(1+R2)…(1+Rn)] power of 1/n −1       
+    # Reference: https://www.investopedia.com/terms/g/geometricmean.asp  
+    # μ geometric=([(1+R1)(1+R2)…(1+Rn)] power of 1/n) −1       
     returns_count=len(returns)
-    """Cumulative geometric mean (compounded return)."""
+    """Cumulative geometric mean (compounded return)"""
     return np.prod((1 + returns))**(1/returns_count) - 1
     
 '''
-#Alternatively method for average daily geometric mean return
+# Alternatively method for the average daily geometric mean return
 def average_geometric_mean(returns: pd.Series, na_strategy: str = "drop") -> float:
-    """Average geometric mean (per-day)."""
+    """Average geometric mean (per-day)"""
     
     if handle_nan == "drop":
         returns = returns.dropna()
@@ -39,21 +39,21 @@ def average_geometric_mean(returns: pd.Series, na_strategy: str = "drop") -> flo
 
 def cumulative_returns_and_alpha(df, as_of_date, fund_col, bench_col, date_col, day_period, na_strategy="keep"):
     
-    #Ensure the inputs is datetime format
+    # Ensure the inputs are the datetime format
     df[date_col] = pd.to_datetime(df[date_col])
     as_of_date = pd.to_datetime(as_of_date)
     output = []
     
     #The for loop may not be the most efficient approach for large datasets, but it serves the purpose for this task.
     for w in day_period:
-        #Get start date based on entered day_period
+        # Get start date based on entered day_period
         start_date = as_of_date - pd.Timedelta(days=w)
-        #Filtering to only keep rows between the start date and as of date
+        # Filtering to only keep rows between the start date and as of date
         period_df = df[(df[date_col] >= start_date) & (df[date_col] <= as_of_date)]
         fund_return = geometric_cumulative_return(period_df[fund_col], na_strategy)
         bench_return = geometric_cumulative_return(period_df[bench_col], na_strategy)
         
-        #Calculate the differences and check if any nan exist
+        # Calculate the differences and check if any nan exist
         alpha = (fund_return - bench_return) if pd.notna(fund_return) and pd.notna(bench_return) else np.nan
         output.append({
             "as_of": as_of_date.strftime("%Y-%m-%d"),
